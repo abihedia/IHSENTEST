d5b73dff14a4e11691a06a2b08315c8f8d9b7964
abihedia <51483423+abihedia@users.noreply.github.com>
2021-06-24 12:34:13 +0100

    Add files via upload

--- cu_sub/models/models.py
+++ cu_sub/models/models.py
@@ -7,23 +7,24 @@ class AccountMove(models.Model):
     _inherit = "account.move"
     _description = "Journal Entry"
 
-    # ajout champs amount_market dans le modele facturation
-    amount_market = fields.Monetary(string='Montant Marché', compute='_get_amount_market_sale')
-    # @api.depends('partner_id', 'state')
-    def _get_amount_market_sale(self):
-        """
-        Calculer Montant Marché par client.
-        """
-        sale_ids = self.env['sale.order'].search([('partner_id', '=', self.partner_id.id), ('state', '=', 'sale')])
-        amount = 0.0
-        for par in sale_ids:
-            amount += par.amount_total
+    # # ajout champs amount_market dans le modele facturation
+    # amount_market = fields.Monetary(string='Montant Marché', compute='_get_amount_market_sale')
+    # # @api.depends('partner_id', 'state')
+    # def _get_amount_market_sale(self):
+    #     """
+    #     Calculer Montant Marché par client.
+    #     """
+    #     sale_ids = self.env['sale.order'].search([('partner_id', '=', self.partner_id.id), ('state', '=', 'sale')])
+    #     amount = 0.0
+    #     for par in sale_ids:
+    #         amount += par.amount_total
+    #
+    #     self.amount_market = amount
+    first_test_field=fields.Char(string='ihsen test *****************')
+    cu_price_subtotal = fields.Monetary(string='Subtotal Cu', store=True, readonly=True,
+                                     currency_field='currency_id')
 
-        self.amount_market = amount
-
-
-
-    @api.depends('invoice_line_ids.x_studio_subtotal')
+    @api.depends('invoice_line_ids.cu_price_subtotal')
     def _get_per_advance_categ_subtot(self):
         """
         Calculer advance.
@@ -42,7 +43,7 @@ class AccountMove(models.Model):
 
                 if line.display_type != 'line_section' and line.display_type != 'line_note':
                     # av_line += line.per_advance_product
-                    av_line += line.x_studio_subtotal
+                    av_line += line.cu_price_subtotal
                     comp_line += 1
                 if line.display_type == 'line_section':
                     if comp_line != 0:
@@ -56,7 +57,7 @@ class AccountMove(models.Model):
                         av_note = av_section
                         av_section -= av_section
                         comp_section = 0
-                        line.x_studio_subtotal = av_note
+                        line.cu_price_subtotal = av_note
                         # line.write({'per_advance_note': av_note
                         #             })
 
